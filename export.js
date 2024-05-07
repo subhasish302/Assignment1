999291b8fa70f0b8684070ab8e9c3d7978b9935b
Subhasish Pathak <162189705+subhasish302@users.noreply.github.com>
2024-05-08 00:38:53 +0530

    Add files via upload

---
+++ export.js
@@ -0,0 +1,5 @@
+const Sum=(a,b)=>a+b;
+const Multiply=(a,b)=>a*b;
+const Division=(a,b)=>a/b;
+const Substraction=(a,b)=>a-b;
+module.exports={Sum,Multiply,Division,Substraction};
---
+++ import.js
@@ -0,0 +1,2 @@
+const Operation=require('./module1Export');
+console.log(Operation.Substraction(3,4));
